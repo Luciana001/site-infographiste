e46ec8ed0be065100586c96f6a4a5974c9fc76bc
Luciana001 <luciana.brancato@gmail.com>
2021-06-29 10:52:28 +0200

    wp-config

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -56,7 +56,7 @@ define( 'DB_PASSWORD', getenv_docker('WORDPRESS_DB_PASSWORD', 'root') );
  */
 
 /** MySQL hostname */
-define( 'DB_HOST', getenv_docker('WORDPRESS_DB_HOST', 'mysql') );
+define( 'DB_HOST', getenv_docker('WORDPRESS_DB_HOST', 'localhost') );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', getenv_docker('WORDPRESS_DB_CHARSET', 'utf8') );
